fe7605ce34bf16f1dbc58946d9d8eb939c58aaf3
Darren Hall <itsdarrenhall@gmail.com>
2020-04-16 22:44:41 +0100

    feat: add banner image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
+import BudirChurchXS from "../images/budir-church-xs.jpg"
+import BudirChurchSm from "../images/budir-church-sm.jpg"
+import BudirChurchMd from "../images/budir-church-md.jpg"
+import BudirChurchLg from "../images/budir-church-lg.jpg"
+import BudirChurchXL from "../images/budir-church-xl.jpg"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -10,7 +16,12 @@ const IndexPage = () => (
       <p>Eighteenth of July<br/>Two Thousand And Twenty One</p>
       <p>B&#xFA;&#xF0;ir, Iceland</p>
     </div>
+    <div className="ratio">
+      <div className="ratio__container">
+        <div className="ratio__padding"></div>
+      </div>
     </div>
+    
   </Layout>
 )
 
