759357c7cf4b0297c40f23e4514e523670da217e
Darren Hall <itsdarrenhall@gmail.com>
2020-04-18 18:02:22 +0100

    feat: update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Oli Dickinson and Darren Hall | 18-07-21 | Búðir Iceland`,
+    description: `Eighteenth of July Two Thousand and Twenty One. Búðir, Iceland`,
+    author: `Darren Hall`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
