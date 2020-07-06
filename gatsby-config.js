fec53c9350d2750ee3ce9a5cc6054bad149bd2ee
Darren Hall <itsdarrenhall@gmail.com>
2020-07-06 21:31:39 +0100

    feat: add robots.txt file

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,12 @@ module.exports = {
           },
         ],
       },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', disallow: ['/'] }]
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
