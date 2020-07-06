186a9f67ff05af42fc19f461ad9cc5ed2583ba8f
Darren Hall <itsdarrenhall@gmail.com>
2020-07-06 21:43:21 +0100

    feat: update custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div className="section">
+      <h2>This page got cold feet</h2>
+      <p>Sorry, no idea where this page has got to. It probably just needs a little time to get itself together. Try going <Link to="/">home</Link> and seeing if it&rsquo;s there.</p>
+    </div>
   </Layout>
 )
 
